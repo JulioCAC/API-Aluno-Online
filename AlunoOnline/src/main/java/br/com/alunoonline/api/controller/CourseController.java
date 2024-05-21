a0dcc3700ad20c4f61b948838e1427b5d5bf960e
JulioCAC <juliocac@gmail.com>
2024-05-21 00:29:52 -0300

    metodo update/put e delete de course criado

--- AlunoOnline/src/main/java/br/com/alunoonline/api/controller/CourseController.java
+++ AlunoOnline/src/main/java/br/com/alunoonline/api/controller/CourseController.java
@@ -5,9 +5,11 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,8 +39,18 @@ public class CourseController {
 	@GetMapping("/{id}")
 	@ResponseStatus(HttpStatus.OK)
 	public Optional<Course> findById(@PathVariable Long id){
-		return courseService.findByid(id);
+		return courseService.findById(id);
 	}
 	
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@RequestBody Course course, @PathVariable Long id) {
+		courseService.update(id, course);
+	}
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void deleteById(@PathVariable Long id) {
+		courseService.deleteById(id);
+	}
 
 }
--- AlunoOnline/src/main/java/br/com/alunoonline/api/service/CourseService.java
+++ AlunoOnline/src/main/java/br/com/alunoonline/api/service/CourseService.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.alunoonline.api.model.Course;
 import br.com.alunoonline.api.repository.CourseRepository;
@@ -22,8 +24,29 @@ public class CourseService {
 		return courseRepository.findAll();
 	}
 	
-	public Optional<Course> findByid(Long id){
+	public Optional<Course> findById(Long id){
 		return courseRepository.findById(id);
 	}
+	
+	public void update(Long id, Course course){
+		Optional<Course> courseFromDb = findById(id);
+		
+		if(courseFromDb.isEmpty()) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Curso não encontrado no banco de dados");
+		}
+		
+		Course courseUpdated = courseFromDb.get();
+		
+		courseUpdated.setName(course.getName());
+		courseUpdated.setTuition(course.getTuition());
+		courseUpdated.setType(course.getType());
+		courseRepository.save(courseUpdated);
+		
+	}
+	
+	public void deleteById(Long id) {
+		courseRepository.deleteById(id);
+	}
+	
 
 }
