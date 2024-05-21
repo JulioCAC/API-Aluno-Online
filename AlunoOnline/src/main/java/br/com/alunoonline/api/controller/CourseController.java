47bbb0a4dfc8936832d67a5f9aa94ce3f60cc2da
JulioCAC <juliocac@gmail.com>
2024-05-20 21:49:36 -0300

    metodos get/read de course criados(findAll e findById)

--- AlunoOnline/src/main/java/br/com/alunoonline/api/controller/CourseController.java
+++ AlunoOnline/src/main/java/br/com/alunoonline/api/controller/CourseController.java
@@ -1,7 +1,12 @@
 package br.com.alunoonline.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +28,17 @@ public class CourseController {
 		courseService.create(course);
 	}
 	
+	@GetMapping("/all")
+	@ResponseStatus(HttpStatus.OK)
+	public List<Course> findAll(){
+		return courseService.findAll();
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Optional<Course> findById(@PathVariable Long id){
+		return courseService.findByid(id);
+	}
 	
 
 }
--- AlunoOnline/src/main/java/br/com/alunoonline/api/service/CourseService.java
+++ AlunoOnline/src/main/java/br/com/alunoonline/api/service/CourseService.java
@@ -1,5 +1,8 @@
 package br.com.alunoonline.api.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +16,14 @@ public class CourseService {
 	
 	public void create(Course course) {
 		courseRepository.save(course);
+	}
+	
+	public List<Course> findAll() {
+		return courseRepository.findAll();
+	}
+	
+	public Optional<Course> findByid(Long id){
+		return courseRepository.findById(id);
 	}
 
 }
